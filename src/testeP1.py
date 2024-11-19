f4c5b9651ff01007b6fd6904b808976f7b3478a1
Guilherme Henrique De Lima Machado <1216504@sga.pucminas.br>
2024-11-19 17:31:23 -0300

    Corrigindo nomeclatura

--- src/testeP1.py
+++ src/testeP1.py
@@ -1,10 +1,8 @@
-from libGrafos import Grafo
-from libGrafos import Aresta
-from libGrafos import Vertice
+from libGrafos import Grafo, Aresta, Vertice
 
 
 def test_grafo():
-    print("Iniciando testes...")
+    print("Testando...")
 
     # Teste: Criação de um grafo com X vértices
     vertices = 5
@@ -25,32 +23,28 @@ def test_grafo():
 
     # Teste: Ponderação e rotulação de arestas
     grafo.ponderar_aresta(0, 1, 5)
-    grafo.rotular_aresta(0, 1, "Edge01")
-    print(f"Aresta (0, 1) rotulada como 'Edge01' com peso 5.")
+    grafo.rotular_aresta(0, 1, "aresta01")
+    print(f"Aresta (0, 1) rotulada como 'aresta01' com peso 5.")
 
     # Teste: Checagem de adjacência entre vértices e arestas
-    print(f"Vertices 0 e 1 são adjacentes? {grafo.checar_adjacencia_vertices(0, 1)}")
-    print(f"Aresta (0, 1) existe? {grafo.checar_existencia_aresta(0, 1)}")
+    print(f"Vertices 0 e 1 são adjacentes? {grafo.sao_adjacentes(0, 1)}")
+    print(f"Aresta (0, 1) existe? {grafo.existe_aresta(0, 1)}")
 
     # Teste: Checagem da quantidade de vértices e arestas
-    print(f"Quantidade de vértices: {grafo.quantidade_vertices()}")
-    print(f"Quantidade de arestas: {grafo.quantidade_arestas()}")
+    print(f"Quantidade de vértices: {grafo.numero_vertices()}")
+    print(f"Quantidade de arestas: {grafo.numero_arestas()}")
 
     # Teste: Checagem de grafo vazio e completo
-    print(f"O grafo está vazio? {grafo.checar_grafo_vazio()}")
-    print(f"O grafo é completo? {grafo.checar_grafo_completo()}")
+    print(f"O grafo está vazio? {grafo.grafo_vazio()}")
+    print(f"O grafo é completo? {grafo.grafo_completo()}")
 
     # Teste: Checagem de conectividade
-    print(f"O grafo é simplesmente conexo? {grafo.checar_simplesmente_conexo()}")
-    print(f"O grafo é semi-fortemente conexo? {grafo.checar_semi_fortemente_conexo()}")
-    print(f"O grafo é fortemente conexo? {grafo.checar_fortemente_conexo()}")
-
-    # Teste: Checagem de quantidade de componentes fortemente conexos (Kosaraju)
-    print(f"Quantidade de componentes fortemente conexos: {grafo.checar_componentes_fortemente_conexos()}")
+    print(f"O grafo é simplesmente conexo? {grafo.e_conexo()}")
+    print(f"O grafo é fortemente conexo? {grafo.kosaraju()}")
 
     # Teste: Checagem de ponte e articulação
-    print(f"O grafo possui ponte? {grafo.checar_ponte()}")
-    print(f"O grafo possui articulação? {grafo.checar_articulacao()}")
+    print(f"O grafo possui ponte? {grafo.e_ponte()}")
+    print(f"O grafo possui articulação? {grafo.e_articulacao()}")
 
     print("Todos os testes executados.")
 
