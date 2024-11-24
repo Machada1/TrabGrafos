5db1414a7017fde3cb2f3029ff1b158df0665fb5
diogo <diogocampos3210@gmail.com>
2024-11-24 15:46:20 -0300

    ta acabando

--- src/testeP1Dir.py
+++ src/testeP1Dir.py
@@ -1,46 +1,20 @@
 from libGrafos import Grafo, Direcionado
 
-Dir = Direcionado(4)
-#Dir.rotular_vertices(["A", "B", "C"])
-#Dir.ponderar_vertices([1,2,3])
+Dir = Direcionado(7)
 
 Dir.adicionar_aresta(0,1)
-Dir.adicionar_aresta(2,3)
-# Dir.adicionar_aresta(2,0)
-# Dir.imprimir_lista_adjacencia()
-# Dir.imprimir_matriz_adjacencia()
-# Dir.imprimir_matriz_incidencia()
-Dir.adicionar_aresta(0,1,"AB",5)
-Dir.adicionar_aresta(1,2,"BC",10)
-Dir.adicionar_aresta(2,0,"CA",1)
+Dir.adicionar_aresta(1,4)
+Dir.adicionar_aresta(4,2)
+Dir.adicionar_aresta(2,1)
+Dir.adicionar_aresta(0,3)
+Dir.adicionar_aresta(4,6)
+Dir.adicionar_aresta(6,5)
+Dir.adicionar_aresta(5,6)
+Dir.adicionar_aresta(2,5)
+Dir.adicionar_aresta(3,5)
 
-Dir.e_conexo()
-
-# Dir.imprimir_lista_adjacencia()
-# Dir.imprimir_matriz_adjacencia()
-# Dir.imprimir_matriz_incidencia()
-
-# Dir.sao_adjacentesA((0,1),(1,2))
-# Dir.sao_adjacentesA((1,2),(2,0))
-
-# Dir.existe_aresta(0,1)
-# Dir.existe_aresta(1,2)
-# Dir.existe_aresta(2,0)
-# Dir.existe_aresta(0,2)
-# Dir.existe_aresta(2,1)
-# Dir.existe_aresta(1,0)
-
-# Dir.numero_arestas()
-# Dir.numero_vertices()
-
-# Dir.grafo_vazio()
-# Dir.grafo_completo()
+Dir.imprimir_lista_adjacencia()
+Dir.imprimir_matriz_adjacencia()
+Dir.imprimir_matriz_incidencia()
 
 Dir.kosaraju()
-
-Dir.conectividade()
-Dir.remover_aresta((2,0))
-Dir.conectividade()
-Dir.remover_aresta((1,2))
-Dir.e_conexo()
-Dir.conectividade()
